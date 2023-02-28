5f29a84ea7ee05dacdfa732a122b30d6e1b35955
Anna Krysenko <krysenkoanna@gmail.com>
2023-02-28 23:20:00 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,35 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Cast from './Cast/Cast';
-import Home from '../Pages/Home/Home';
-import Layout from './Layout/Layout';
-import MovieDetails from '../Pages/MovieDetails/MovieDetails';
-import Movies from '../Pages/Movies/Movies';
-import Reviews from './Reviews/Reviews';
+import { Loader } from './Loader/Loader';
+// import Cast from './Cast/Cast';
+// import Home from '../Pages/Home/Home';
+// import Layout from './Layout/Layout';
+// import MovieDetails from '../Pages/MovieDetails/MovieDetails';
+// import Movies from '../Pages/Movies/Movies';
+// import Reviews from './Reviews/Reviews';
+
+const Movies = lazy(() => import('../Pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../Pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Layout = lazy(() => import('./Layout/Layout'));
+const Home = lazy(() => import('../Pages/Home/Home'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Suspense>
   );
 };
 
